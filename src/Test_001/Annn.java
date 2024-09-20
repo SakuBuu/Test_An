fbf0d0bdf70e4164dd719d9868a3e12af44f2474
SakuBuu <vvel14003@gmail.com>
2024-09-20 12:32:33 +0530

    new_anbu

---
+++ src/Test_001/Annn.java
@@ -0,0 +1,15 @@
+package Test_001;
+
+public class Annn {
+
+	public static void main(String[] args) {
+		
+		int a = 234;
+		int b = 343;
+		 int c = a=b;
+		 System.out.println("c =" +c);
+				
+
+	}
+
+}
